9f764c375f46f59ff733ef149179a24ca83c5d26
Vahid <v.torabian@gmail.com>
2021-10-24 16:41:55 +0330

    //

--- Lawmingo/SALE.sql
+++ Lawmingo/SALE.sql
@@ -1,12 +1,13 @@
 DECLARE @PRICE INT , @START DATETIME  , @END DATETIME , @TYPE INT, @WALLET INT
 
-SET @START = '2021-06-23 00:00:00.00' --START DATE
-SET @END = '2021-07-23 23:59:00.00' --END DATE
+SET @START = '2021-06-22 00:00:00.00' --START DATE
+SET @END = '2021-07-22 23:59:00.00' --END DATE
 
 
 SET @PRICE =1 -- FREE
 SET @TYPE = 2 --SMART/TEMPLATE/ALL
 SET @WALLET = 0 --WALLET
+
 
 -- SELECT COUNT(UC.UserContractID) COUNT , SUM(PL.Amount) SUM
 --   FROM [lawmingo_DB].[PsmUser].[UserContracts] UC
@@ -30,13 +31,19 @@ SET @WALLET = 0 --WALLET
 --         AND CR.[Time] > @START
 --         AND CR.[Time] < @END
 
-select count(PaymentLogid)  , sum(Amount) Contracts
+select count(PaymentLogid) as Total  , sum(Amount) PaidContracts
 from [lawmingo_DB].[PsmUser].[PaymentLogs] pl
       WHERE Amount IS NOT NULL AND PL.IsSuccessful = 1 and ConsultationOrderID is NULL AND Wallet = 0
         AND pl.[Time] >= @START
         AND pl.[Time] <= @END
 
- 
+
+SELECT COUNT(UserContractID) as FreeDownload from  [lawmingo_DB].[PsmUser].UserContracts uc
+      JOIN  [lawmingo_DB].[PsmUser].Contract c on c.ContractID = uc.ContractID
+            WHERE c.Price = 0 AND (Content is NOT NULL OR Word is not null)
+                  AND uc.[Time] >= @START
+                  AND uc.[Time] <= @END
+
 select count(PaymentLogid)  , sum(Amount) Consultations
 from [lawmingo_DB].[PsmUser].[PaymentLogs] pl
       WHERE Amount IS NOT NULL AND PL.IsSuccessful = 1 and ConsultationOrderID is not null AND Wallet = 0
@@ -45,7 +52,7 @@ from [lawmingo_DB].[PsmUser].[PaymentLogs] pl
 
  
         
-select sum(Amount) Total
+select sum(Amount) TotalSale
 from [lawmingo_DB].[PsmUser].[PaymentLogs] pl
       WHERE Amount IS NOT NULL AND PL.IsSuccessful = 1 AND Wallet = 0
         AND pl.[Time] >= @START
--- Noafarin/NoafarinEditUser.sql
+++ Noafarin/NoafarinEditUser.sql
@@ -6,30 +6,32 @@ SELECT
 @AffiliateID = '32a5a823-c76f-4b34-b4be-802d780021f0',
  @Refrence = 'Noafarin',
   @DateTime = '2020-10-31 11:40:34.4306135',
-   @PhoneNumber = '09918470949',
+   @PhoneNumber = '09125435102',
     @RefrenceID = '11-54-25-6671'
 
-update [lawmingo_DB].[PsmUser].[AspNetUsers]
-    SET 
+-- update [lawmingo_DB].[PsmUser].[AspNetUsers]
+--     SET 
 
-    AffiliateID = @AffiliateID ,
-     Refrence = @Refrence ,
-      RefrenceID = @RefrenceID
-    OUTPUT  inserted.Id
-    WHERE PhoneNumber = @PhoneNumber
+--     AffiliateID = @AffiliateID ,
+--      Refrence = @Refrence ,
+--       RefrenceID = @RefrenceID
+--     OUTPUT  inserted.Id
+--     WHERE PhoneNumber = @PhoneNumber
 
 select * from [lawmingo_DB].[PsmUser].[AspNetUsers]
 WHERE  UserName= @PhoneNumber
 
 -- Select rows from a Table or View '[PaymentLogs]' in schema '[dbo]'
 
-SELECT * FROM [lawmingo_DB].[PsmUser].[PaymentLogs]
-WHERE  ApplicationUserId= 'e96aebdd-2727-492e-837e-55f9adf7455f'
+SELECT Amount , Name , pl.[Time] FROM [lawmingo_DB].[PsmUser].[PaymentLogs] pl
+left join [lawmingo_DB].[PsmUser].[UserContracts] uc on uc.UserContractID = pl.UserContractID
+left join [lawmingo_DB].[PsmUser].[Contract] c on c.ContractID = uc.ContractID
+WHERE  pl.ApplicationUserId= '2e1f5f6e-9b13-4ee7-8ace-0375d1b92e96'
 GO
 
 -- update PaymentLogs
 -- set Amount = 2000000
--- WHERE ApplicationUserId = 'شسیشسیشسیشسیdca35255-c853-4c87-ad4a-da35838ab74'
+-- WHERE ApplicationUserId = '2e1f5f6asdasdasde-9b13-4ee7-8ace-0375d1b92e96'
 
 -- INSERT INTO PaymentLogs
 -- (
